fe7c4fd6915ca3b9cafc1574d06328c3791ba5dc
RV Odoya <ekszodiac@gmail.com>
2019-04-22 16:35:54 +0800

    1st Change

--- AppiumTest/UnitTest1.cs
+++ AppiumTest/UnitTest1.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
 
 namespace AppiumTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        AndroidDriver<AndroidElement> driver;
+        DesiredCapabilities options ;
+
+
+        [SetUp]
+        public void InitDriver()
         {
+            options = new DesiredCapabilities();
+            options.SetCapability("platformName", "Android");         
+            options.SetCapability("deviceName", "Google Pixel XL - 9.0 - API 28");
+            options.SetCapability("appPackage", "com.android.mms.ui");
+            options.SetCapability("appActivity", "ComposeMessageActivity");
+
+            driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), options);
+
+            Console.WriteLine("Device Connected");
+        }
+
+        [Test]    
+        public void test1()
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(driver);
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        [TearDown]
+        public void closeDriver()
+        {
+            driver.Quit();
         }
     }
 }
